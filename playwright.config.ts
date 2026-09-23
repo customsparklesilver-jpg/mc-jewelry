c55e753f9501d8854b3d70e40f8dfee3707bc39a
customsparklesilver-jpg <customsparklesilver@gmail.com>
2026-09-23 12:14:56 -0700

    Update playwright.config.ts

--- playwright.config.ts
+++ playwright.config.ts
@@ -4,7 +4,9 @@ export default defineConfig({
 testDir: './tests',
 testMatch: '**/*.test.ts',
 use: {
-baseURL:process.env.PLAYWRIGHT_TEST_BASE_URL || 'https://mc-jewelry-a3c90e379ea9cc7a2c4e.o2.myshopify.dev',
+baseURL: process.env.PLAYWRIGHT_TEST_BASE_URL || 'https://mc-jewelry-a3c90e379ea9cc7a2c4e.o2.myshopify.dev',
+},
+projects: [
 {
 name: 'chromium',
 use: { ...devices['Desktop Chrome'] },
