f3da97fca80af4e41d0299dc7b924eb73837129c
customsparklesilver-jpg <customsparklesilver@gmail.com>
2026-09-22 17:57:46 -0700

    Update cart.test.ts

--- tests/cart.test.ts
+++ tests/cart.test.ts
@@ -6,7 +6,7 @@ test.describe('Cart', () => {
   test('From home to checkout flow', async ({page}) => {
     // Home => Collections => First collection => First product
     await page.goto(`/`);
-    await page.locator(`header nav a:text-is("Catalog")`).click();
+    await page.locator('a:text("Catalog")`).first().click();
     await page.locator('[data-test=collection-grid] a >> nth=0').click();
     await page.locator(`[data-test=product-grid] a  >> nth=0`).click();
 
