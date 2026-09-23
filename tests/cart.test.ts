619e406a8eb7b8b0a23fa79c4bebaf3bb9cc96b9
customsparklesilver-jpg <customsparklesilver@gmail.com>
2026-09-22 17:33:47 -0700

    Update cart.test.ts

--- tests/cart.test.ts
+++ tests/cart.test.ts
@@ -6,8 +6,7 @@ test.describe('Cart', () => {
   test('From home to checkout flow', async ({page}) => {
     // Home => Collections => First collection => First product
     await page.goto(`/`);
-    await page.locator(`header nav a:text-is("Collections")`).click();
-    await page.locator(`[data-test=collection-grid] a  >> nth=0`).click();
+    await page.locator(`header nav a:text-is("Catalog")`).click();
     await page.locator(`[data-test=product-grid] a  >> nth=0`).click();
 
     const firstItemPrice = normalizePrice(
@@ -38,7 +37,7 @@ test.describe('Cart', () => {
 
     // Close cart drawer => Products => First product
     await page.locator('[data-test=close-cart]').click();
-    await page.locator(`header nav a:text-is("Products")`).click();
+    await page.locator(`header nav a:text-is("Catalog")`).click();
     await page.locator(`[data-test=product-grid] a  >> nth=0`).click();
 
     const secondItemPrice = normalizePrice(
