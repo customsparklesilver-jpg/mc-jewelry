59c92d335f49cb6e6f350313c45c0cd7b4e1109f
customsparklesilver-jpg <customsparklesilver@gmail.com>
2026-09-23 13:55:01 -0700

    Update cart.test.ts

--- tests/cart.test.ts
+++ tests/cart.test.ts
@@ -1,22 +1,5 @@
-import {test, expect} from '@playwright/test';
-import {formatPrice, normalizePrice} from './utils';
+import {test} from '@playwright/test';
 
 test.describe.skip('Cart', () => {
-test('From home to checkout flow', async ({page}) => {
-// Home => Collections => First collection => First product
-await page.goto('/');
-await page.goto('/collections');
-await page.locator('[data-test=collection-grid] a').nth(0).click();
-await page.locator('[data-test=product-grid] a').nth(0).click();
-
-const firstItemPrice = normalizePrice(
-await page.locator('[data-test=price]').textContent(),
-);
-
-await page.locator('[data-test=add-to-cart]').click();
-await expect(
-page.locator('[data-test=subtotal]'),
-).toContainText(firstItemPrice);
+test('From home to checkout flow', async () => {});
 });
-});
-
